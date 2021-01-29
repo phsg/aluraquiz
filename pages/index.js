aa444999e2d8c0234fe2de7667bfe5449e72c745
Pedro Henrique <phsgmaster@gmail.com>
2021-01-29 00:04:12 -0300

    Aula 04

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,9 @@ import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
   const rota = useRouter();
@@ -36,21 +28,25 @@ export default function Home() {
             <h1>The legend</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function (evento) {
+            <form onSubmit={(evento) => {
               evento.preventDefault();
               rota.push(`/quiz?name=${nome}`);
             }}
             >
-              <input
-                onChange={function (infoDoEvento) {
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infoDoEvento) => {
                   setNome(infoDoEvento.target.value);
                 }}
                 placeholder="Diz seu nome"
+                value={nome}
               />
-              <button type="submit" disabled={nome.length === 0}>
-                Jogar
-                {nome}
-              </button>
+              <Button
+                type="submit"
+                disabled={nome.length === 0}
+              >
+                {`Jogar ${nome}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
